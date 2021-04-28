7d4533a426485cdb7294a1521be4aa857dd081ef
Shiela Sabellita <shiela.sabellita@gmail.com>
2021-04-28 14:46:00 +0800

    add login

--- analytics_app/urls.py
+++ analytics_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+from analytics_app.views import LoginView
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
-    path('', views.landing, name="landing"),
+    path('', TemplateView.as_view(template_name='analytics_app/landing.html'), name="landing"),
+    path('login/', LoginView.as_view(), name="login"),
+    path('loginform/', LoginView.as_view(), name="login_form"),
 ]
--- analytics_app/views.py
+++ analytics_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.views.generic import TemplateView
+
+
+class LoginView(TemplateView):
+    template_name = 'analytics_app/login.html'
+
+    
 
 
 def landing(request):
