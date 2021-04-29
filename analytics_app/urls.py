534a63d89817eeca069e5ab3cfbfa20884589539
Shiela Sabellita <shiela.sabellita@gmail.com>
2021-04-29 17:28:26 +0800

    add login function and add signup and dashboard page

--- analytics_app/urls.py
+++ analytics_app/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from analytics_app.views import LoginView
+from analytics_app.views import LoginView, logout, DashboardView
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='analytics_app/landing.html'), name="landing"),
-    path('login/', LoginView.as_view(), name="login"),
+    path('login/', TemplateView.as_view(template_name='analytics_app/login.html'), name="login"),
+    path('dashboard/', DashboardView.as_view(), name="dashboard"),
+    path('signup/', TemplateView.as_view(template_name='analytics_app/signup.html'), name="signup"),
+    
+    path('logout/', logout, name="logout"),
     path('loginform/', LoginView.as_view(), name="login_form"),
 ]
--- analytics_app/views.py
+++ analytics_app/views.py
@@ -1,13 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponse, JsonResponse
-from django.views.generic import TemplateView
+from django.contrib.auth.models import User, auth
+from django.views.generic import TemplateView, CreateView
+from django.views import View
+
+class LoginView(View):
+    template_name = 'analytics_app/dashboard.html'
+
+    def post(self, request, *args, **kwargs):
+        crdntls = {
+            "usr": request.POST['usr'],
+            "pwd": request.POST['pwd']
+        }
+        user = auth.authenticate(username = crdntls['usr'], password=crdntls['pwd'])
+        if user:
+            auth.login(request, user)
+            return redirect("dashboard")
+        else:
+            messages.info(request, "Invalid credentials")
+            return redirect("login")
+            
+class DashboardView(TemplateView):
+    template_name = "analytics_app/dashboard.html"
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return render(request, self.template_name, {})
+        else:
+            messages.info(request, "Access Denied")
+            return redirect("login")
+        
 
 
-class LoginView(TemplateView):
-    template_name = 'analytics_app/login.html'
+class SignupView(CreateView):
+    model = User
 
-    
-
-
-def landing(request):
-    return render(request, "template.html")
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
